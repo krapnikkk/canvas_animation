2c7d767f688a7ad9258cff1e7b9cd7ccbeccb159
krapnik <krapnik@qq.com>
2020-03-31 23:59:26 +0800

    add ballsHitTest

--- assets/script/utils.js
+++ assets/script/utils.js
@@ -49,3 +49,20 @@ utils.getDistance = function (x1, y1, x2, y2) {
 utils.ballTest = function (ball1, ball2) {
     return utils.getDistance(ball1.x, ball1.y, ball2.x, ball2.y) < ball1.r + ball2.r;
 }
+
+utils.checkBallBounce = function (ball, W, H, bounce) {
+    if (ball.x - ball.r <= 0) {
+        ball.x = ball.r;
+        ball.vx *= bounce;
+    } else if (ball.x + ball.r >= W) {
+        ball.x = W - ball.r;
+        ball.vx *= bounce;
+    }
+    if (ball.y - ball.r <= 0) {
+        ball.y = ball.r;
+        ball.vy *= bounce;
+    } else if (ball.y + ball.r >= H) {
+        ball.y = H - ball.r;
+        ball.vy *= bounce;
+    }
+};
